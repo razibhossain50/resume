a22f74af1d4473e8a93ef60d7ab42e53606d93ef
razibmahmud50 <razibmahmud50@gmail.com>
2024-02-26 14:18:36 +0600

    footer link target issue fix

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -3,6 +3,7 @@ export default function Footer(){
     return (
         <footer className="text-center">
         <p className="text-sm dark:text-light/70"> @ Copyright  <a
+                target="__blank"
                 href="https://www.facebook.com/razibhossain80/"
                 className="inline-block border-b border-b-transparent text-primary transition hover:border-b-primary hover:text-blue-600">
                 Razib Hossain
